d96a0b01fab16e56e454d316a3b66bfa0116f1b9
Emil Jönsson <ungothar97@gmail.com>
2020-05-14 14:39:52 +0200

    Man kan selecta när man klickar på dem men vill man det

--- src/gui/SlotLabel.java
+++ src/gui/SlotLabel.java
@@ -1,9 +1,48 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class SlotLabel extends ColoredLabel {
-    public SlotLabel() {
+	public boolean selected = false;
+    public SlotLabel()  {
         super("                    ", Color.WHITE, RIGHT);
+        this.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				setBackground(Color.YELLOW);
+				selected = true;
+			}
+		});
+    }
+    public SlotLabel(String content)  {
+    	super(content, Color.WHITE, RIGHT);
     }
 }
