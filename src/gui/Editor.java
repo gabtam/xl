7de1b142533360acccdc4763099288edb345d133
Emil Jönsson <ungothar97@gmail.com>
2020-05-15 13:51:58 +0200

    Constructor change editor

--- src/gui/Editor.java
+++ src/gui/Editor.java
@@ -1,20 +1,35 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JTextField;
 
+import model.Sheet;
+
+
 @SuppressWarnings("deprecation")
-public class Editor extends JTextField implements Observer {
-    public Editor() {
+public class Editor extends JTextField implements Observer, ActionListener{
+    public Editor(Sheet sheet) {
         setBackground(Color.WHITE);
+        addActionListener( this );
     }
 
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
-		
+		setText(arg.toString());
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		 System.out.println(this.getText());
+		 this.setText("");
 	}
 }
